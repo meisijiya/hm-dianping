2c79b380507590ff7dfd778b8636d9116aaeb1fb
MeiSiJiYa <2292360909@qq.com>
2025-09-22 16:24:21 +0800

    updated 基于session方案更改为Redis方案，对UserServiceImpl中的sendcode方法和login方法进行了修改

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.hmdp.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.bean.copier.CopyOptions;
+import cn.hutool.core.lang.UUID;
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.LoginFormDTO;
@@ -11,9 +13,17 @@ import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.RegexUtils;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -25,7 +35,8 @@ import javax.servlet.http.HttpSession;
  */
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
-
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
     @Override
     public Result sendCode(String phone, HttpSession session) {
         // 1.校验手机号
@@ -36,8 +47,9 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         // 3.符合，生成验证码
         String code = RandomUtil.randomNumbers(6);
 
-        // 4.保存验证码到 session
-        session.setAttribute("code",code);
+        // 4.保存验证码到 redis
+        //可以加个业务前缀，防止其他业务的验证码冲突
+        stringRedisTemplate.opsForValue().set(LOGIN_CODE_KEY+phone,code,LOGIN_CODE_TTL, TimeUnit.MINUTES);
         // 5.发送验证码
         log.debug("发送短信验证码成功，验证码："+code);
         // 返回ok
@@ -52,25 +64,35 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
             // 2.如果不符合，返回错误信息
             return Result.fail("手机号格式错误！");
         }
-        // 3.校验验证码
-        Object cacheCode = session.getAttribute("code");
-        String code = loginForm.getCode();
-        if(cacheCode == null || !cacheCode.toString().equals(code)){
+        // 3.从Redis获取验证码并校验
+        String cacheCode=stringRedisTemplate.opsForValue().get(LOGIN_CODE_KEY+phone);
+        String code=loginForm.getCode();
+        if(cacheCode == null || !cacheCode.equals(code)){
             //3.不一致，报错
             return Result.fail("验证码错误");
         }
-        // 4.验证码一致，查询用户
+        // 4.验证码一致，查询用户 select * from tb_user where phone = ?
         User user=query().eq("phone",phone).one();
         if(user == null){
             // 5.用户不存在，创建
             user =  createUserWithPhone(phone);
         }
-        //6.保护用户信息到session
-        UserDTO userDTO = new UserDTO();
-        BeanUtil.copyProperties(user, userDTO);
-        session.setAttribute("user",userDTO );
-        //7.返回ok
-        return Result.ok();
+        // 6.保护用户信息到Redis中
+        // 6.1随机生成token，作为登录令牌
+        String token= UUID.randomUUID().toString();
+        // 6.2将User对象转为HashMap存储
+        UserDTO userDTO=BeanUtil.copyProperties(user,UserDTO.class);
+        Map<String,Object> userMap=BeanUtil.beanToMap(userDTO,new HashMap<>(),
+                CopyOptions.create()
+                        .setIgnoreNullValue(true)
+                        .setFieldValueEditor((fieldName,fieldValue)->fieldValue.toString()));
+        // 6.3Hash存储中添加token和DTO对象
+        String tokenKey=LOGIN_USER_KEY+token;
+        stringRedisTemplate.opsForHash().putAll(tokenKey,userMap);
+        // 6.4设置token的过期时间
+        stringRedisTemplate.expire(LOGIN_USER_KEY+token,LOGIN_USER_TTL, TimeUnit.MINUTES);
+        // 7.返回token
+        return Result.ok(token);
     }
 
     private User createUserWithPhone(String phone) {
--- src/main/java/com/hmdp/utils/LoginInterceptor.java
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -4,22 +4,32 @@ import cn.hutool.Hutool;
 import cn.hutool.core.bean.BeanUtil;
 import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+//手动new出来的类，所以不能直接注入bean，需要手动注册到spring容器中
 public class LoginInterceptor implements HandlerInterceptor {
+    //通过构造函数创建
+    private StringRedisTemplate redisTemplate;
+    public LoginInterceptor(StringRedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
 
     }
+
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        //1.获取session
-        HttpSession session = request.getSession();
-        //2.获取session中的用户
+        //TODO 1.获取请求头中的token
+
+        //2.TODO 2.基于Token获取Redis中的用户
         Object user = session.getAttribute("user");
         //3.判断用户是否存在
         if(user != null) {
@@ -27,9 +37,12 @@ public class LoginInterceptor implements HandlerInterceptor {
             response.setStatus(401);
             return false;
         }
-        //5.存在,保存用户信息到ThreadLocal
+        //TODO 5.将查询到的Hash数据转为UserDTO对象
         UserDTO userDTO = BeanUtil.copyProperties(user, UserDTO.class);
         UserHolder.saveUser(userDTO);
+        //TODO 6.存在，将UserDTO对象存入ThreadLocal中
+
+        //TODO 7.刷新token有效期
         //6.放行
         return true;
     }
